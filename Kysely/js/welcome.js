195d3ae4414fdfe1270d19a5a394d2e400094fb4
Jani Veihtola <jani.veihtola@gmail.com>
2018-05-10 07:42:46 +0300

    Add files via upload

--- Kysely/js/welcome.js
+++ Kysely/js/welcome.js
@@ -1,3 +1,7 @@
+
+let video = document.getElementById("backgroundVideo");
+video.playbackRate = 0.5;
+
 function fadeout(){
     $("body").fadeOut( 1000, function() {
         window.location.assign("form.html");
